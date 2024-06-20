3616679aad22211a3172f6478307048c9073dab6
dharmrathore <dharm.rathore90gmail.com>
2024-06-20 19:54:00 +0530

    chan

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const HeroBanner = () => {
     return (
         <>
-        <section className='w-full md:py-5' id='home'>
-            <div className='max-w-7xl mx-auto px-4'>
+        <section className='w-full md:py-5 min-h-screen relative bg-fixed z-[1] bg-no-repeat bg-cover bg-center flex items-center justify-center before:opacity-85 before:bg-blue-500 opacity-45 before:w-full before:h-full before:absolute before:top-0 before:left-0' id='home'  style={{ backgroundImage: "url('images/bg-hero.jpg')" }}>
+            <div className='max-w-7xl mx-auto px-4 relative z-[2]'>
                 <div className='flex bg-gray-200 bg-gradient-to-tl text-[#1f436e] p-4 py-10 md:p-36 rounded-xl border w-full items-center justify-center flex-col'>
                     <h1 className='text-xl md:text-4xl lg:text-6xl uppercase font-semibold'>I'M <span className='text-transparent textstroke animate-pulse transition-all ease-in inline-block duration-200 bg-clip-text  bg-gradient-to-r from-cyan-500 to-violet-500'>Dev </span> Rathore</h1>
                     <ul className='mt-4 bg-white rounded-lg flex items-center gap-2 md:gap-3 flex-wrap px-3 md:px-4 py-3 '>
