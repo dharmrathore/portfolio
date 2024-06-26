b1aa5c5098955759231009c2cec1a547b343d202
dharmrathore <dharm.rathore90gmail.com>
2024-06-26 10:40:59 +0530

    imags

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -2,7 +2,8 @@ export default function manifest() {
     return {
         name: 'Dharmendra Rathore',
         short_name: 'Dev Rathore',
-        description: 'An Application Development Environment for Dharmendra Rathore',
+        description: 'Empowering Digital Transformation: UI/UX Design, Webflow Development, and Front-End Engineering by Dharmendra Rathore',
+        start_url: '/',
         start_url: '/',
         display: 'standalone',
         background_color: '#fff',
