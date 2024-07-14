5a1243b915fcdd1ec14721f49676a3359e724074
dharmrathore <dharm.rathore90@gmail.com>
2024-07-14 18:20:38 +0530

    form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,59 @@
+"use client";
 import { iconsSvg } from '@/app/utils/Iconsvg'
-import Link from 'next/link'
-import React from 'react'
+import React, { useState } from "react";
 
 const Footer = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        email:'',
+        message: '',
+    });
+
+    const handleInput = (e) =>{
+        setState({...state, [e.target.name]: e.target.value});
+        //const { name, value } = e.target;
+        // setState(prevState => ({
+        //     ...prevState,
+        //     [name]: value,
+        // }));
+    };
+
+    const hendleSubmitForm = async (e) =>{
+        e.preventDefault();
+
+        try{
+            const response = await fetch('',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: state.name,
+                    email: state.email,
+                    message: state.message,
+                }),
+            });
+            if(!response.ok){
+                throw new Error('Network response was not ok');
+            }
+            // const data = await response.json();
+
+            alert('Email sent successfully!');
+            
+            setState({
+                name: '',
+                email:'',
+                message: '',
+            });
+        }
+        catch (error){
+            console.error("Error sending email", error)
+            alert('Failed to send email, please try again later');
+        }
+    };
+
+
     return (
         <>
             <footer className='w-full  md:py-5 bg-gray-50' id='contact'>
@@ -32,20 +83,20 @@ const Footer = () => {
                         </div>
                         <div className="basis-full md:basis-7/12">
                             <div className='bg-white border p-4 md:p-10 rounded-lg'>
-                                <form action='/api/contact' method='POST'>
+                                <form onSubmit={(e)=> hendleSubmitForm(e)}>
                                 <div className='flex flex-col md:flex-row w-full gap-4 md:gap-8 mb-4 md:mb-8'>
                                     <div className='block w-full md:w-6/12'>
                                         <label className='mb-2 inline-block text-sm md:text-base' htmlFor="Your Name">Your Name</label>
-                                        <input type="text" name="name" className='text-sm md:text-base w-full border p-3 rounded-lg theme-color' id="name" placeholder='Enter Your Name...'  required/>
+                                        <input onChange={(e) => {handleInput(e)}} value={state.name} type="text" name="name" className='text-sm md:text-base w-full border p-3 rounded-lg theme-color' id="name" placeholder='Enter Your Name...'  required/>
                                     </div>
                                     <div className='block w-full md:w-6/12'>
                                         <label className='mb-2 inline-block text-sm md:text-base' htmlFor="Your Email">Your Email</label>
-                                        <input type="email" name="email" id="email" className='text-sm md:text-base w-full border p-3 rounded-lg theme-color' placeholder='Enter Your Email...' required/>
+                                        <input onChange={(e) => {handleInput(e)}} value={state.email} type="email" name="email" id="email" className='text-sm md:text-base w-full border p-3 rounded-lg theme-color' placeholder='Enter Your Email...' required/>
                                     </div>
                                 </div>
                                 <div className="block text-start mb-4 md:mb-8">
                                     <label className='mb-2 inline-block text-sm md:text-base' htmlFor="Your Message">Your Message</label>
-                                    <textarea name="message" id="message" rows={4} className='text-sm md:text-base w-full border p-3 rounded-lg theme-colorr' placeholder='Write Your Message...' required></textarea>
+                                    <textarea onChange={(e) => {handleInput(e)}} value={state.message} name="message" id="message" rows={4} className='text-sm md:text-base w-full border p-3 rounded-lg theme-colorr' placeholder='Write Your Message...' required></textarea>
                                 </div>
                                 <button type='submit' className="p-3 text-sm md:text-base md:p-4 border bg-[#1f436e] bg-gradient-to-r  hover:from-pink-500 hover:to-violet-500 text-white transition-all flex items-center gap-1 rounded-md hover:shadow-lg hover:opacity-90">
                                     Send Me Message
