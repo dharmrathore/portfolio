95511124276f02da196d85b916873bf83ca92eec
dharmrathore <dharm.rathore90gmail.com>
2024-10-23 11:18:08 +0530

    dev change

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,14 @@
-export default function robots() {
-    return {
-      rules: {
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/',
+  generateRobotsTxt: true, 
+  robotsTxtOptions: {
+    policies: [
+      {
         userAgent: '*',
         allow: '/',
         disallow: '/private/',
       },
-      sitemap: `${process.env.NEXT_PUBLIC_BASE_URL/sitemap.xml}`,
-    }
-  }
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  },
+};
